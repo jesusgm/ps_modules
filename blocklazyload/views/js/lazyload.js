1b5bc4072a2cdad4e06e0e47ae2a5d14c3f95818
Jesús Gándara Martínez <jesusalxen@gmail.com>
2016-08-07 12:37:39 +0200

    Change load event to document ready instead window load

--- blocklazyload/views/js/lazyload.js
+++ blocklazyload/views/js/lazyload.js
@@ -1,10 +1,17 @@
 $(document).ready(function(){
+  /*
+   *  Change the src attribute of target images to data-original attribute
+   *  for the lazyload plugin
+   */
   $(img_selector).not(img_exclude_selector).each(function(){
     $(this).attr('data-original', $(this).attr('src')).removeAttr('src');
-  })
-});
-$(window).bind("load", function() {
-    $(img_selector).not(img_exclude_selector).lazyload({
-      effect : 'fadeIn'
-    });
+  });
+
+  /*
+   *  Init the lazyload plugin to target images
+   */
+  $(img_selector).not(img_exclude_selector).lazyload({
+    effect : 'fadeIn'
+  });
+  
 });
